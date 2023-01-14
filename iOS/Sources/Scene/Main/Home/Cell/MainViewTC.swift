ca36c68ae4ac807ddb37a829ce1410822987dce6
김대희 <0824dh@naver.com>
2023-01-15 02:30:17 +0900

    feat :: MainViewTC 일부 수정

--- iOS/Sources/Scene/Main/Home/Cell/MainViewTC.swift
+++ iOS/Sources/Scene/Main/Home/Cell/MainViewTC.swift
@@ -16,6 +16,12 @@ final class MainViewTC: BaseTC {
         super.init(style: .default, reuseIdentifier: "Main")
     }
 
+    private let backView = UIView().then {
+        $0.backgroundColor = AttentionAsset.Assets.gray50.color
+        $0.setShadow(radius: 8)
+        $0.layer.cornerRadius = 8
+    }
+
     private let titleLabel = UILabel().then {
         $0.font = UIFont(name: AttentionFontFamily.Pretendard.medium.name, size: 15.0)
         $0.textColor = AttentionAsset.Assets.gray800.color
@@ -32,28 +38,43 @@ final class MainViewTC: BaseTC {
     }
 
     override func configureVC() {
+        titleLabel.text = self.title
+        dateLabel.text = self.date
+        sponsorLabel.text = self.sponsor
         contentView.backgroundColor = AttentionAsset.Assets.gray50.color
         contentView.layer.cornerRadius = 8
-        contentView.setShadow(radius: 8)
+        contentView.addSubview(backView)
+        [
+            titleLabel,
+            dateLabel,
+            sponsorLabel
+        ].forEach { backView.addSubview($0) }
     }
 
     override func setLayout() {
+        backView.snp.makeConstraints {
+            $0.top.equalTo(10)
+            $0.bottom.equalTo(-10)
+            $0.leading.trailing.equalToSuperview().inset(24)
+        }
+
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(10)
-            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.leading.trailing.equalTo(0).inset(15)
             $0.height.equalTo(24)
         }
 
         dateLabel.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom)
-            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.leading.trailing.equalTo(0).inset(15)
             $0.height.equalTo(24)
         }
 
         sponsorLabel.snp.makeConstraints {
             $0.top.equalTo(dateLabel.snp.bottom)
-            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.leading.trailing.equalTo(0).inset(15)
             $0.height.equalTo(24)
+            $0.bottom.equalTo(-10)
         }
     }
 }
